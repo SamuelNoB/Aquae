f7281bd769d8f70b0acd2bf850f680246f5d5186
Samuel <samuelnbacelar@hotmal.com>
2021-07-08 14:37:48 -0300

    .

--- Aquae/settings.py
+++ Aquae/settings.py
@@ -124,9 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'core', 'static/assets')
 
-STATICFILES_DIRS = [
-    "core/static/assets"
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
