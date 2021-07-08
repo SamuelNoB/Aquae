890507f96e1a8a05afba81c13a46fbf6730b9b9d
Samuel <samuelnbacelar@hotmal.com>
2021-07-08 14:44:56 -0300

    hotfix

--- Aquae/urls.py
+++ Aquae/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
     path('simulacao/', include('simulacao.urls', namespace='simulacao'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
