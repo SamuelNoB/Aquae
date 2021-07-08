c2637fad8391a38ad7911c87191fe4aac2cdedfe
Samuel <samuelnbacelar@hotmal.com>
2021-07-08 14:35:54 -0300

    .

--- Aquae/settings.py
+++ Aquae/settings.py
@@ -120,13 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+#PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'core', 'static/assets')
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static/assets'),
-)
+STATICFILES_DIRS = [
+    "core/static/assets"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
