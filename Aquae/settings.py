f2a03e63dff84122696680ddff802e6f3c7ff1cc
Samuel <samuelnbacelar@hotmal.com>
2021-07-08 14:28:28 -0300

    static

--- Aquae/settings.py
+++ Aquae/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'core', 'static/assets')
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'static/assets'),
 )
 
 # Default primary key field type
