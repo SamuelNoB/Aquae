72049481fcd33bc4c951114b847a9b93277f4300
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-02-17 13:43:36 -0300

    Escalabilidade de funcoes do RAC

--- core/static/assets/js/simulacao.js
+++ core/static/assets/js/simulacao.js
@@ -3,6 +3,11 @@ function add(accumulator, a) {
     return accumulator + a;
 }
 
+/*Arredonda um valor para x casas decimais*/
+function round_x(float, decimals) {
+    const p10 = 10**decimals
+    return Math.round(float*p10 + Number.EPSILON) / (p10)
+}
 
 /*Transforma um valor tipo float em formatacao de R$*/
 function parseReal(float){
@@ -77,47 +82,19 @@ function clean_data(labels, values) {
     return ({cleaned_labels, cleaned_values})
 }
 
-
-function plot(x, y, type='bar', custom_opt='') {
-    const clean = clean_data(labels = x, values = y)
-    
-    /*Deleta a canvas antiga para nao bugar a visualizacao*/
-    document.getElementById('SimulChart').remove()
-    
-    /*Nova canvas*/
-    const ChartDiv =  document.getElementById('ChartDiv')
-
-    const canvas = ChartDiv.appendChild(document.createElement("canvas"))
-
-    canvas.setAttribute('id', 'SimulChart')
-    const ctx = canvas.getContext('2d');
-    
-    const SimulChart = new Chart(ctx, {
-        type: type,
-        data: {
-            labels: clean.cleaned_labels,
-            
-            datasets: [{
-                label: 'Economia de Água m³/ano',
-                data: clean.cleaned_values,
-                fill: false,
-                backgroundColor: 'rgba(54, 162, 235, 1)',
-                borderColor: 'rgba(54, 162, 235, 1)',
-                borderWidth: 1
-            }],
-        },
-        options: custom_opt   
-    });
-}
-
-/*Retorna a possivel economia de cada demanda*/
-function data_adjust(oferta_indicadores, demanda_indicadores) {
-    const economia = []
-    const oferta_total = oferta_indicadores.reduce(add, 0) * 12
-    for (let i = 0; i < demanda_indicadores.length; i++) {
-        economia.push(Math.min(demanda_indicadores[i] * 12, oferta_total))
+/*Faz a soma dos indicadores da demanda*/
+function soma_dem(indicadores, nomes) {
+    let soma = 0
+    for (let i = 0; i < indicadores.length; i++) {
+        let mensal = indicadores[i]
+        if (nomes[i] != "Irrigação de jardins") {
+            soma = soma + mensal*12
+        } else {
+            /*A irrigacao so e contabilizada nos meses de estiagem*/
+            soma = soma + mensal*6
+        }
     }
-    return economia
+    return soma
 }
 
 /*Refaz o grafico, e outros elementos da pagina, toda vez que ocorre uma interacao com checkbox*/
@@ -126,62 +103,6 @@ function re_plot(id) {
     const index = id.replace(/[^0-9]/g, '')
     const checkbox = document.getElementById(id)
     const indicador = document.getElementById(id.replace('check', 'indicador')).value
-
-}
-
-/*Cria elementos da parte financeira e capacidade necessaria*/
-function create_tabs(demanda, tarifa, juros) {
-    /*Seletor de capacidade minima*/
-    let volume_cap = Math.max(...volumes_cap)
-    for (let i = 0; i < volumes_cap.length; i++) {
-        if (volumes_cap[i] > demanda) {
-            volume_cap = volumes_cap[i]
-            break
-        }
-    }
-    /*Seletor de caixa minima*/
-    let volume_caixa = Math.max(...volumes_caixa)
-    for (let i = 0; i < volumes_caixa.length; i++) {
-        if (volumes_caixa[i] > demanda) {
-            volume_caixa = volumes_caixa[i]
-            break
-        }
-    }
-    document.getElementById("Caixa_volume").innerHTML = `${String(volume_caixa)} L`
-
-    const custo_op_final = Math.round((financeiro_cap[volume_cap][1] + custo_op_bomba + Number.EPSILON)*100)/100
-    document.getElementById("custo_operacional").innerHTML = parseReal(custo_op_final) + ' R$/ano'
-
-    const custo_cap = Math.round((financeiro_cap[volume_cap][0] + custo_bomba + financeiro_caixa[volume_caixa] + Number.EPSILON)*100)/100
-    document.getElementById("Custo_cap").innerHTML = parseReal(custo_cap)
-
-    document.getElementById("Capacidade").innerHTML = volume_cap
-    
-    const Beneficio = Math.round((tarifa * demanda * 30 * 12 / 1000 - custo_op_final + Number.EPSILON)*100) / 100
-    document.getElementById("Beneficio").innerHTML = parseReal(Beneficio)
-
-    let Payback = Math.ceil(custo_cap / Beneficio)
-    Payback_span = document.getElementById("Payback")
-    if (Payback < 0) {
-        Payback = 'Inviável!';
-        Payback_span.setAttribute("style", "color:red")
-    } else {
-        Payback_span.setAttribute("style", "color:black")
-    }
-    Payback_span.innerHTML = Payback
-
-
-    /*Determina VPL*/
-    const VPL_parcelas = []
-    for (let i = 1; i <= 30; i++) {
-        const parcela = Beneficio / ((1 + juros)**i)
-        VPL_parcelas.push(parcela)
-    }
-    const VPL = Math.round((VPL_parcelas.reduce(add, 0) - custo_cap + Number.EPSILON)*100)/100
-    const vpl_span = document.getElementById("VPL")
-    vpl_span.innerHTML = parseReal(VPL)         
-    if (VPL < 0) {vpl_span.setAttribute("style", "color:red")}
-    else {vpl_span.setAttribute("style", "color:black")}
 
 }
 
