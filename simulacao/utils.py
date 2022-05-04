5045dede111f49fcdc2c4fbb073d1003a4168836
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-05-04 13:45:06 -0300

    Correcao do bug da faixa de volume pulando 1

--- simulacao/utils.py
+++ simulacao/utils.py
@@ -1,10 +1,11 @@
 import requests as req
 from lxml import html
 
+
 def get_dollar():
-    res = req.get('https://economia.awesomeapi.com.br/json/all/USD-BRL')
+    res = req.get("https://economia.awesomeapi.com.br/json/all/USD-BRL")
     dollar_data = res.json()
-    dollar = float(dollar_data['USD']['low'])
+    dollar = float(dollar_data["USD"]["low"])
     return round(dollar, 2)
 
 
@@ -14,9 +15,9 @@ def get_ni_ipca():
     res = req.get(url)
     doc = html.fromstring(res.content)
 
-    N_indice = doc.xpath('//td')[-518].text
-    
-    return float(N_indice.replace('.', '').replace(',', '.'))
+    N_indice = doc.xpath("//td")[-518].text
+
+    return float(N_indice.replace(".", "").replace(",", "."))
 
 
 def get_tarifa_caesb():
@@ -26,18 +27,27 @@ def get_tarifa_caesb():
     # O site da CAESB tem uma incosistencia na estrutura
     aliquota = '//*[@id="conceitual"]/div[3]/center/table[1]/tbody/tr[3]/td[4]/div'
     vol_faixa = '//*[@id="conceitual"]/div[3]/center/table[1]/tbody/tr[3]/td[3]/div'
-    
+
     tarifa = doc.xpath(aliquota)[0].text.lstrip()
     vol_faixa = int(doc.xpath(vol_faixa)[0].text.lstrip())
-    
+
     vol0 = 0
-    tarifas = [{"min": vol0, "max": vol0+vol_faixa, "tarifa": float(tarifa.replace(",", "."))}]
+    tarifas = [
+        {
+            "min": vol0,
+            "max": vol0 + vol_faixa,
+            "tarifa": float(tarifa.replace(",", ".")),
+        }
+    ]
     vol0 += vol_faixa + 1
 
-
-    for i in range(4,9):
-        aliquota = f'//*[@id="conceitual"]/div[3]/center/table[1]/tbody/tr[{i}]/td[4]/div/span'
-        vol_faixa = f'//*[@id="conceitual"]/div[3]/center/table[1]/tbody/tr[{i}]/td[3]/div'
+    for i in range(4, 9):
+        aliquota = (
+            f'//*[@id="conceitual"]/div[3]/center/table[1]/tbody/tr[{i}]/td[4]/div/span'
+        )
+        vol_faixa = (
+            f'//*[@id="conceitual"]/div[3]/center/table[1]/tbody/tr[{i}]/td[3]/div'
+        )
         tarifa = doc.xpath(aliquota)[0].text.lstrip()
 
         vol_faixa = doc.xpath(vol_faixa)
@@ -45,10 +55,14 @@ def get_tarifa_caesb():
             vol_faixa = 9999999
         else:
             vol_faixa = int(vol_faixa[0].text.lstrip())
-        
-        tarifas.append({"min": vol0, "max": vol0+vol_faixa-1, "tarifa": float(tarifa.replace(",", "."))})
-        vol0 += vol_faixa + 1
-    
+
+        tarifas.append(
+            {
+                "min": vol0,
+                "max": vol0 + vol_faixa - 1,
+                "tarifa": float(tarifa.replace(",", ".")),
+            }
+        )
+        vol0 += vol_faixa
 
     return tarifas
-        
