62defc4f6352b88848aa3f9ec64440be2f271850
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-05-08 20:30:04 -0300

    Formulario pede estado e salva na db

--- simulacao/models.py
+++ simulacao/models.py
@@ -46,7 +46,42 @@ class DemandasDeAgua(models.Model):
 class Simulacao(models.Model):
 
     RESIDENCIA_CHOICES = ((0, "casa"), (1, "apartamento"))
+    ESTADO_CHOICES = (
+        ("Acre (AC)", "AC"),
+        ("Alagoas (AL)", "AL"),
+        ("Amapá (AP)", "AP"),
+        ("Amazonas (AM)", "AM"),
+        ("Bahia (BA)", "BA"),
+        ("Ceará (CE)", "CE"),
+        ("Distrito Federal (DF)", "DF"),
+        ("Espírito Santo (ES)", "ES"),
+        ("Goiás (GO)", "GO"),
+        ("Maranhão (MA)", "MA"),
+        ("Mato Grosso (MT)", "MT"),
+        ("Mato Grosso do Sul (MS)", "MS"),
+        ("Minas Gerais (MG)", "MG"),
+        ("Pará (PA)", "PA"),
+        ("Paraíba (PB)", "PB"),
+        ("Paraná (PR)", "PR"),
+        ("Pernambuco (PE)", "PE"),
+        ("Piauí (PI)", "PI"),
+        ("Rio de Janeiro (RJ)", "RJ"),
+        ("Rio Grande do Norte (RN)", "RN"),
+        ("Rio Grande do Sul (RS)", "RS"),
+        ("Rondônia (RO) ", "RO"),
+        ("Roraima (RR)", "RR"),
+        ("Santa Catarina (SC)", "SC"),
+        ("São Paulo (SP)", "SP"),
+        ("Sergipe (SE)", "SE"),
+        ("Tocantins (TO)", "TO"),
+    )
 
+    estado = models.CharField(
+        verbose_name="Estado (UF)",
+        max_length=24,
+        choices=ESTADO_CHOICES,
+        default="Distrito Federal (DF)",
+    )
     tipo_residencia = models.IntegerField(
         verbose_name="Tipo da residencia", choices=RESIDENCIA_CHOICES
     )
