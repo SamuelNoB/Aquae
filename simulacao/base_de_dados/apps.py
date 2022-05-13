5e7860e0345ec34fef4308b5f89cd8a8614408b6
Joao_Vitor_Dickmann <joaovitordickmann25@gmail.com>
2022-05-13 19:22:50 -0300

    teste

--- simulacao/base_de_dados/apps.py
+++ simulacao/base_de_dados/apps.py
@@ -2,6 +2,9 @@ from django.apps import AppConfig
 
 
 class BaseDeDadosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'simulacao.base_de_dados'
-    verbose_name = 'Base de dados'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "simulacao.base_de_dados"
+    verbose_name = "Base de dados"
+
+
+x = 1
