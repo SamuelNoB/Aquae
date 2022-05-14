5320737f08354215488fff3a8438a04c3e7d5462
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-05-14 16:15:45 -0300

    Meses de 1 a 12

--- simulacao/base_de_dados/models.py
+++ simulacao/base_de_dados/models.py
@@ -44,18 +44,18 @@ class TarifaDeAgua(models.Model):
 
 class IndicePluviometrico(models.Model):
     MESES_CHOICES = (
-        (0, "Janeiro"),
-        (1, "Fevereiro"),
-        (2, "Março"),
-        (3, "Abril"),
-        (4, "Maio"),
-        (5, "Junho"),
-        (6, "Julho"),
-        (7, "Agosto"),
-        (8, "Setembro"),
-        (9, "Outubro"),
-        (10, "Novembro"),
-        (11, "Dezembro"),
+        (1, "Janeiro"),
+        (2, "Fevereiro"),
+        (3, "Março"),
+        (4, "Abril"),
+        (5, "Maio"),
+        (6, "Junho"),
+        (7, "Julho"),
+        (8, "Agosto"),
+        (9, "Setembro"),
+        (10, "Outubro"),
+        (11, "Novembro"),
+        (12, "Dezembro"),
     )
 
     ano = models.IntegerField("Ano")
