4cdf3ece5b89e5c7e3ff43a1a9364a38a82d7fbc
Joao_Vitor_Dickmann <joaovitordickmann25@gmail.com>
2023-03-20 13:30:42 -0300

    ligando debug mode para conferir erro

--- Aquae/settings.py
+++ Aquae/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
